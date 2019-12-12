5d3999d5534eaafc9fb04a9a422040b5b46645a4
Viktor Ahlqvist <viktor@rxbc.se>
2019-12-12 12:57:42 +0100

    Add isort to sort the imports better

--- todo_extractor/__init__.py
+++ todo_extractor/__init__.py
@@ -5,5 +5,5 @@ See README.md for more intructions on how to run the script.
 
 __version__ = "0.2.1"
 
-from .todo_extractor import search_folder  # noqa
 from .cli import main  # noqa
+from .todo_extractor import search_folder  # noqa
--- todo_extractor/todo_extractor.py
+++ todo_extractor/todo_extractor.py
@@ -4,7 +4,6 @@ import re
 import subprocess
 import typing
 
-
 TODO_REGEXP = "^.*TODO:.*"
 
 
